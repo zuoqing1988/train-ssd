6ccbb32d65da487721f5865ae9ff26ff1bd1e8af
Zuo Qing <zuoqing1988@aliyun.com>
2019-04-29 10:12:39 +0800

    train_imagenet可选network

--- train_imagenet.bat
+++ train_imagenet.bat
@@ -1,3 +1,3 @@
 set MXNET_CUDNN_AUTOTUNE_DEFAULT=0
-python classify_example\train_imagenet.py --lr 0.01 --end_epoch 16 --prefix model/mymodel --lr_epoch 8,14,100 --batch_size 32 --thread_num 8 --frequent 100 
+python classify_example\train_imagenet.py --network mymodel --lr 0.01 --end_epoch 16 --prefix model/mymodel --lr_epoch 8,14,100 --batch_size 32 --thread_num 8 --frequent 100 
 pause 
