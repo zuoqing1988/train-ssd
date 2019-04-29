64d2a17ef8abfebdea8c6ef0f120829abb6e11f2
Zuo Qing <zuoqing1988@aliyun.com>
2019-04-29 13:49:16 +0800

    修改默认参数

--- train_ssd_hand.bat
+++ train_ssd_hand.bat
@@ -1,3 +1,3 @@
 set MXNET_CUDNN_AUTOTUNE_DEFAULT=0
-python train.py --lr 0.01 --network mymodel --epoch 7 --lr-steps 200,300 --end-epoch 400 --data-shape 192 --batch-size 64 --frequent 10 --num-class 1 --class-names hand --train-path gesture_train.rec --val-path gesture_val.rec --prefix model/ssd_hand
+python train.py --lr 0.01 --network mymodel --epoch 7 --lr-steps 200,300 --end-epoch 400 --data-shape 192 --batch-size 32 --frequent 10 --num-class 1 --class-names hand --train-path gesture_train.rec --val-path gesture_val.rec --prefix model/ssd_hand
 pause 
